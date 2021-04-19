e4f50063519db2eef467dad54286bb6c3ad83f3f
TJun-Jie <tjunjie2000@gmail.com>
2021-04-19 20:24:33 +0800

    first commit

---
+++ frontend/components/Header.js
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="bar">
+        <a href="/">Sick fits</a>
+      </div>
+      <div className="sub-bar">
+        <p>Search</p>
+      </div>
+    </header>
+  );
+}
