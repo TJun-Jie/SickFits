fdd9fb8b99ccad00496f2b5af1cb472a939c5bce
TJun-Jie <tjunjie2000@gmail.com>
2021-04-21 21:01:07 +0800

    added styled component server side rendering config at _document.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -10,7 +10,7 @@ Router.events.on('routeChagneError', () => NProgress.done());
 export default function MyApp({ Component, pageProps }) {
   return (
     <Page>
-      // eslint-disable-next-line react/jsx-props-no-spreading
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
       <Component {...pageProps} />
     </Page>
   );
