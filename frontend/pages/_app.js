57d12ea03b0d315c46a5d351944bdd35ce0fd541
TJun-Jie <tjunjie2000@gmail.com>
2021-04-21 20:39:36 +0800

    added eslint config

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,4 +1,11 @@
 import Page from '../components/Page';
+import Router from 'next/router';
+import '../components/styles/nprogress.css';
+import NProgress from 'nprogress';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChagneError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,10 +1,14 @@
 import Document, { Html, Head, NextScript, Main } from 'next/document';
 
+import NProgress from 'nprogress';
+
+import 'nprogress'
+
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en-CA">
-        {/* <Head></Head> */}
+      <Html>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
